696e39fbf9629b15a526ec7669c13477f9e0da6e
ammar <ammar.ali.haidar.1990@gmail.com>
2023-04-29 19:09:37 +0300

    done

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,7 +9,7 @@ import {ShoppingCartIcon} from '@heroicons/react/24/outline'
 
 const Product = ({ id, description, title, rate, image, price }) => {
   const { user,isLoading } = useSelector(getUser);
-  const { cart } = useSelector(getCart);
+  const { cart,isLoading:cartLoad } = useSelector(getCart);
   const itemExist = cart.findIndex((el) => el.id === id);
   const item = cart.find((el) => el.id === id);
 const toAdd = async ()=> {
@@ -58,13 +58,13 @@ const toAdd = async ()=> {
 
         <p className="py-2 italic">${price}</p>
       </div>
-    {!isLoading && user &&<> {itemExist === -1 && (
-        <button onClick={toAdd} className="bg-orange-400 flex gap-2 py-1  text-white pl-2 pr-4 rounded-r-full text-sm max-w-fit mb-10">
+    {!isLoading &&  user &&<> {itemExist === -1 && (
+        <button onClick={toAdd} className={`bg-orange-400 flex gap-2 py-1  text-white pl-2 pr-4 rounded-r-full text-sm max-w-fit duration-1000 ${cartLoad ? 'opacity-0' : 'opacity-100'} mb-10`}>
       Add to cart  <ShoppingCartIcon className="h-5 text-white" />  {" "}
         </button>
       )}
       {item && (
-        <div className="p-2 border text-sm w-full border-r-0 border-l-0 mb-10 flex items-center gap-4 justify-center">
+        <div className={`p-2 border text-sm w-full border-r-0 border-l-0 mb-10 flex items-center gap-4 justify-center  duration-1000 ${cartLoad ? 'opacity-0' : 'opacity-100'} `}>
           <span>in cart</span>
           <button onClick={addHandle} className="p-2 font-bold text-xl">+</button> {item.quantity}
           <button onClick={minusHandle} className="p-2 font-bold text-xl">-</button>
